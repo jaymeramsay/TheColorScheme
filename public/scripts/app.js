e24164065f785934bb85ec537f1702b2763ce518
jaymeramsay <jayme.ramsay@gmail.com>
2018-05-21 17:32:52 -0500

    sets up scripts folder with app.js for js compilation with babel and sets up index.html

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('Color Scheme App.js is running!');
+
+var template = React.createElement(
+  'p',
+  null,
+  'Here is some JSX!'
+);
+
+ReactDOM.render(template, document.getElementById('app'));
